f2e30a7d0eb8048a14bdd3c5eb358d0ed579bf53
makorn645 <makornthawateugeneemery@gmail.com>
2018-05-07 12:24:50 +0530

    zenfone3 customver

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,8 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_EXCLUDE_TWRPAPP := true
+TW_DEVICE_VERSION := makorn645
 
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
