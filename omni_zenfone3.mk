284f16eef8d336ddb2a1c8821cd31f3d041ada44
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2019-03-29 11:12:36 +0000

    prepare for pie twrp

--- omni_zenfone3.mk
+++ omni_zenfone3.mk
@@ -3,9 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := lineage_zenfone3
+PRODUCT_NAME := omni_zenfone3
 PRODUCT_DEVICE := zenfone3
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
